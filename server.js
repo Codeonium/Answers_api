5c931e8786972f1e15e295b690bcf3203e860541
codeonium <codeon2020@gmail.com>
2021-05-28 13:42:25 +0100

    added server info

---
+++ server.js
@@ -0,0 +1,10 @@
+const jsonServer = required('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('api.json');
+const middleware = jsonServer.default();
+const port = process.env.PORT || 3000;
+
+server.use(middleware);
+server.use(router);
+
+server.listen(port);
